779e5d750c8227b5b284fea244d3ca69725ecbc9
silent4life <neo2004pal@gmail.com>
2026-01-19 19:40:56 +1200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,11 +1,4 @@
-<script src="https://elyffrgnxwnllmxzfaof.supabase.co"></script>
-<script>
-  const SUPABASE_URL = "sb_publishable_5zLd1wu50YN56VsMXhSdXQ_zr8UNmsL";
-  const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVseWZmcmdueHdubGxteHpmYW9mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg3ODQ1NDgsImV4cCI6MjA4NDM2MDU0OH0.ZWWYNkyVubb0sxP-7qP0ectfzUKLQhIl6HsrrspY5_8";
+const SUPABASE_URL = "https://elyffrgnxwnllmxzfaof.supabase.co";
+const SUPABASE_KEY = "sb_publishable_5zLd1wu50YN56VsMXhSdXQ_zr8UNmsL";
 
-  window.supabase = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-  );
-</script>
-
+window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
